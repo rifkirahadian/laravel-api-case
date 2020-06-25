1a878f9d3a39018d16f4d6ca64065c4c92e49ee1
Rifki Rahadian <rrahadian94@gmail.com>
2020-06-26 05:41:03 +0700

    Trait: Responser

---
+++ app/Traits/Responser.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Traits;
+
+trait Responser {
+	protected function successResponse($data, $message)
+	{
+		return response()->json(compact('message', 'data'))->send();
+	}
+
+	protected function badRequest($message)
+	{
+        $response = [
+            'error'     => 'Bad Request',
+            'message'   => $message
+        ];
+        
+        response()->json($response, 400)->send();
+        dd();
+	}
+}

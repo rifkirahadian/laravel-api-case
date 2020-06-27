126023e48b86cac2832dffe6cbddfa2c37520b98
Rifki Rahadian <rrahadian94@gmail.com>
2020-06-27 15:19:42 +0700

    Model-create: ProductTransactionDetail

---
+++ database/migrations/2020_06_27_081750_create_product_transaction_details_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTransactionDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_transaction_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_transaction_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->unsignedInteger('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_transaction_details');
+    }
+}
